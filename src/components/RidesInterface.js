2ef5a11e646c4f11817ce6182d1c8f4508bd783b
Abhinav4279 <sharmaabhinav4279@gmail.com>
2022-04-08 09:51:17 +0530

    Add state to component

--- src/components/RidesInterface.js
+++ src/components/RidesInterface.js
@@ -1,9 +1,39 @@
+import { useEffect, useState } from 'react'
+
+const URL = '';
+
 const RidesInterface = () => {
     //TODO: Define state
+    const [rides, setRides] = useState([]);
+
+    // TODO: update data on changing ride category
+    // const [ride_category, setRide_category] = useState('nearest');
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const resp = await fetch(URL);
+            const upd_rides = await resp.json();
+
+            setRides(upd_rides);
+            // setRide_category
+        };
+
+        fetchData();
+    }
+    , []);
+
+    const handleFilterUpdate = () => {
+
+    }
+
+    const handleRideCategoryUpdate = () => {
+
+    }
+
     return (
-        <>
-            <Navbar />
-            <Rides />
-        </>
+        <div>
+            <RideType rides={rides} onClick={handleRideCategoryUpdate}/>
+            <Rides rides={rides} onClick={handleFilterUpdate}/>
+        </div>
     );
 }
