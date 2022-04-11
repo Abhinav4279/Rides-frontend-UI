c97fe4f99406c0074edeb1f6aa8818c447207e1b
Abhinav4279 <sharmaabhinav4279@gmail.com>
2022-04-11 11:04:07 +0530

    Fix station path array

--- src/components/Rides.js
+++ src/components/Rides.js
@@ -19,7 +19,8 @@ const Ride = ({ ride, origin }) => {
         <div>
             <p>Ride id: {ride.id}</p>
             <p>Origin Station: {ride.origin_station_code}</p>
-            <p>station_path: {ride.destination_station_code}</p>
+            <p>station_path: [{ride.station_path.map((stn) => `${stn}, `)}]</p>
+            <p>Date: {ride.date.toString()}</p>
             <p>Distance: {ride.station_path.reduce((prev, curr) => Math.min(Math.abs(origin - curr), prev), 1e9)}</p>
             {/* <p>station_path: {ride.city}</p> */}
         </div>
