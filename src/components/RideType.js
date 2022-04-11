221008779145a4b89e9fbd808577076444d7808b
Abhinav4279 <sharmaabhinav4279@gmail.com>
2022-04-11 11:03:26 +0530

    Add RideType css styling

--- src/components/RideType.js
+++ src/components/RideType.js
@@ -1,9 +1,14 @@
-const RideType = ({ onClick }) => {
+import './RideType.css'
+import { useState } from 'react'
+
+const RideType = ({ onClick}) => {
+    const [active, setActive] = useState(0);
+
     return (
         <div>
-            <button onClick={() => onClick('nearest')}>Nearest Rides</button>
-            <button onClick={() => onClick('upcoming')}>Upcoming Rides</button>
-            <button onClick={() => onClick('past')}>Past Rides</button>
+            <button className={(active === 0)? "active": null} onClick={() => {onClick('nearest'); setActive(0);}}>Nearest Rides</button>
+            <button className={(active === 1)? "active": null} onClick={() => {onClick('upcoming'); setActive(1);}}>Upcoming Rides</button>
+            <button className={(active === 2)? "active": null} onClick={() => {onClick('past'); setActive(2);}}>Past Rides</button>
         </div>
     );
 }
