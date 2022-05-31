d43a816c9e8ab5e4e3d3ab26a7cb2aca15b182ed
Abhinav Sharma <54229503+Abhinav4279@users.noreply.github.com>
2022-05-31 16:12:44 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import './Header.css';
 const Header = ({ user }) => {
     return (
         <div className='container'>
-            <h1>Edvora</h1>
+            <h1>RIDES</h1>
             <div>
                 <h2>{user.name}</h2>
                 {user.profile_key && <img src={user.profile_key} alt='user' />}     {/* conditional rendering */}     
